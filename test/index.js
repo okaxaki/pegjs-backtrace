ea7dc81c3ce6dde1b6683233f9726cb016d5f8c1
localhostdotdev <localhostdotdev@protonmail.com>
2019-05-13 15:50:31 +0200

    nice tests (success/failure)

--- test/index.js
+++ test/index.js
@@ -1,25 +1,44 @@
 "use strict";
 
-var parser = require("./test");
-var Tracer = require("../src/main.js");
+let parser = require("./test");
+let Tracer = require("../src/main.js");
 
-var source = process.argv[2] || "2 + 10 / (3 / 4)";
+let success = process.argv[2] || "1 / 1";
+let failure = process.argv[3] || "oh no";
 
-var tracer = new Tracer(source, {
-  useColor: true,
-  showTrace: true,
-  showSource: true,
-  showFullPath: true,
-  hiddenPaths: ["primary/.*"],
-  // on: ["rule.enter", "rule.match", "rule.fail", "error"]
-});
+// on: ["rule.enter", "rule.match", "rule.fail", "error"]
+let tracerOptions = [
+  {
+    // showTrace: true,
+    hiddenPaths: ["*/primary/*", "*/ws/*"],
+    showFullPath: true,
+    showSource: true
+  }
+]
 
-try {
-  let result = parser.parse(source, { tracer: tracer });
-  console.log(tracer.getParseTreeString());
-  console.log(tracer.getBacktraceString())
+let json = (input) => JSON.stringify(input, null, 2)
+
+tracerOptions.forEach((options) => {
+  let tracerSuccess = new Tracer(success, options);
+  let tracerFailure = new Tracer(failure, options);
+
+  let result = parser.parse(success, { tracer: tracerSuccess });
+
+  console.log(`\n   SUCCESS FOR ${success}\n\noptions:${json(options)}`)
+  console.log("\n== getParseTreeString() ==\n")
+  console.log(tracerSuccess.getParseTreeString());
+  console.log("\n== getBacktraceString() ==\n")
+  console.log(tracerSuccess.getBacktraceString())
+  console.log("\n== result ==\n")
   console.log(result)
-} catch (e) {
-  console.log(e.message);
-  console.log(tracer.getBacktraceString());
-}
+
+  try {
+    parser.parse(failure, { tracer: tracerFailure });
+  } catch (e) {
+    console.log(`\n   FAILURE FOR ${failure}\n\noptions: ${json(options)}`)
+    console.log("\n== error message ==\n")
+    console.log(e.message);
+    console.log("\n== getBacktraceString() ==\n")
+    console.log(tracerFailure.getBacktraceString());
+  }
+})
